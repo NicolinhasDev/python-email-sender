70b9826e5023f0801acf4522b96f80fc11575559
NicolinhasDev <108492620+NicolinhasDev@users.noreply.github.com>
2022-09-12 23:15:20 -0300

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,26 @@
+from email import message
+from multiprocessing import context
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = "Email From Python"
+body = "This is a test email from Python!"
+sender_email = "nicolinhas.dev@gmail.com"
+receiver_email = "nicolinhas.dev@gmail.com"
+password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending Email!")
+with smtplib.SMTP_SSL("smtp.gmail.com", 0, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Success")
